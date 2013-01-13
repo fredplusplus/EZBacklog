6999715d546fed1b2add294459d46f8b6d687a48
Fan Zhang <fredplusplus@gmail.com>
2013-01-12 19:21:35 -0800

    localize date on client

--- war/static/js/dashboard.js
+++ war/static/js/dashboard.js
@@ -3,6 +3,8 @@ Dashboard = Backbone.View.extend({
 		this.render();
 	},
 	render : function() {
+		var time = new Date(dashboardData.backlogger.joinDate +" UTC");
+		dashboardData.backlogger.joinDate = time.getDate() + '/' + (time.getMonth()+1) + '/' + time.getFullYear();
 		var dashboardTemplate = _.template($("#dashboardTemplate").html(), dashboardData);
 		this.$el.html(dashboardTemplate);
 	}
