4b3c1ea3b104d7c0b4f6539c1ad86138c080568a
Fan Zhang <fredplusplus@gmail.com>
2013-01-27 14:45:10 -0800

    Create data object for one related item
    actually just a related link

--- src/webplus/ezbacklog/model/RelatedItem.java
+++ src/webplus/ezbacklog/model/RelatedItem.java
@@ -1,5 +1,66 @@
 package webplus.ezbacklog.model;
 
-public class RelatedItem {
+import java.util.Date;
 
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+/**
+ * Data object for one related item.
+ * 
+ */
+@PersistenceCapable
+public class RelatedItem {
+	@PrimaryKey
+	@Persistent
+	private Long id;
+	@Persistent
+	private Long itemId;
+	@Persistent
+	private String creatorEmail;
+	@Persistent
+	private Date createDate;
+	@Persistent
+	private String relatedItem;
+
+	public String getCreatorEmail() {
+		return creatorEmail;
+	}
+
+	public void setCreatorEmail(String creatorEmail) {
+		this.creatorEmail = creatorEmail;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(Long itemId) {
+		this.itemId = itemId;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public String getRelatedItem() {
+		return relatedItem;
+	}
+
+	public void setRelatedItem(String relatedItem) {
+		this.relatedItem = relatedItem;
+	}
 }
