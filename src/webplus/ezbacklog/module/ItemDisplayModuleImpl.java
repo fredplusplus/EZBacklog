36d4a170e94d00cb46918f73c3abf8da8540563a
Fan Zhang <fredplusplus@gmail.com>
2013-02-14 22:31:15 -0800

    display items based on team belongings
    http://leanbacklog.appspot.com/f/items/6001/13009

--- src/webplus/ezbacklog/module/ItemDisplayModuleImpl.java
+++ src/webplus/ezbacklog/module/ItemDisplayModuleImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.jdo.JDOObjectNotFoundException;
 import javax.jdo.PersistenceManager;
@@ -13,8 +14,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import webplus.ezbacklog.model.Backlogger;
 import webplus.ezbacklog.model.Item;
+import webplus.ezbacklog.model.Team;
 import webplus.ezbacklog.module.interfaces.BackloggerModule;
 import webplus.ezbacklog.module.interfaces.ItemDisplayModule;
+import webplus.ezbacklog.module.interfaces.TeamModule;
 import webplus.ezbacklog.service.PMF;
 import webplus.ezbacklog.values.ItemLevel;
 import webplus.ezbacklog.values.ItemStatus;
@@ -23,6 +26,8 @@ public class ItemDisplayModuleImpl implements ItemDisplayModule {
 
 	@Autowired
 	private BackloggerModule backloggerModule;
+	@Autowired
+	private TeamModule teamModule;
 	private Map<Long, List<Item>> getItemByParentIdResultCache;
 	private Map<Long, List<Item>> getItemByLevelResultCache;
 
@@ -38,7 +43,13 @@ public class ItemDisplayModuleImpl implements ItemDisplayModule {
 		} else {
 			String filter = "ownerEmail == '%s' && itemLevel == " + level;
 			filter = String.format(filter, backloggerModule.getCurrencyBacklogger().getEmail());
-			List<Item> items = query(filter);
+			List<Item> personalItems = query(filter);
+			String teamFilter = constructTeamFilter();
+			List<Item> teamItems = null;
+			if (teamFilter != null && !teamFilter.isEmpty()) {
+				teamItems = query("itemLevel == " + level + teamFilter);
+			}
+			List<Item> items = mergeItems(personalItems, teamItems);
 			getItemByLevelResultCache.put(level, items);
 			return items;
 		}
@@ -62,7 +73,6 @@ public class ItemDisplayModuleImpl implements ItemDisplayModule {
 			return getItemByParentIdResultCache.get(parentId);
 		} else {
 			String filter = null;
-			List<Item> items = null;
 			if (parentId <= 0) {
 				filter = "ownerEmail == '%s' && itemLevel == " + ItemLevel.PROJECT;
 			} else {
@@ -70,12 +80,20 @@ public class ItemDisplayModuleImpl implements ItemDisplayModule {
 			}
 			String statusFilter = constructStatusFilter();
 			if (statusFilter == null) {
-				return items;
+				return null;
 			} else if (statusFilter.length() > 0) {
 				filter += " && " + statusFilter;
 			}
 			filter = String.format(filter, backloggerModule.getCurrencyBacklogger().getEmail(), parentId);
-			items = query(filter);
+			List<Item> personalItems = query(filter);
+			String teamFilter = constructTeamFilter();
+			List<Item> teamItems = null;
+			if (teamFilter != null && !teamFilter.isEmpty()) {
+				filter = filter.replace("ownerEmail == ", "ownerEmail != ");
+				filter += constructTeamFilter();
+				teamItems = query(filter);
+			}
+			List<Item> items = mergeItems(personalItems, teamItems);
 			getItemByParentIdResultCache.put(parentId, items);
 			return items;
 		}
@@ -117,5 +135,43 @@ public class ItemDisplayModuleImpl implements ItemDisplayModule {
 		Query query = pm.newQuery(Item.class);
 		query.setFilter(filter);
 		return (List<Item>) query.execute();
+	}
+
+	private String constructTeamFilter() {
+		String format = "&& (%s)";
+		List<Team> teams = teamModule.getAllTeamsForCurrentUser(false);
+		StringBuilder sb = new StringBuilder();
+		if (teams == null || teams.isEmpty()) {
+			return "";
+		} else {
+			for (int i = 0; i < teams.size(); i++) {
+				if (i == 0) {
+					sb.append("teamId == " + teams.get(i).getTeamName().getId());
+				} else {
+					sb.append("|| teamId == " + teams.get(i).getTeamName().getId());
+				}
+			}
+		}
+
+		return String.format(format, sb.toString());
+	}
+
+	private List<Item> mergeItems(List<Item> personalItems, List<Item> teamItems) {
+		Map<Long, Item> itemMap = new HashMap<Long, Item>();
+		if (personalItems != null) {
+			for (Item item : personalItems) {
+				itemMap.put(item.getId(), item);
+			}
+		}
+		if (teamItems != null) {
+			for (Item item : teamItems) {
+				itemMap.put(item.getId(), item);
+			}
+		}
+		List<Item> items = new ArrayList<Item>();
+		for (Entry<Long, Item> entry : itemMap.entrySet()) {
+			items.add(entry.getValue());
+		}
+		return items;
 	}
 }
